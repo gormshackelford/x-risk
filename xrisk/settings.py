b658432a05ff9b42f6405699164ca9a8d9ff7cea
gormshackelford <gorm.shackelford@gmail.com>
2018-08-24 14:40:39 +0100

    Added SECURE_SSL_REDIRECT

--- xrisk/settings.py
+++ xrisk/settings.py
@@ -17,6 +17,9 @@ import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# The SSL certificate is installed for production, but not for development.
+SECURE_SSL_REDIRECT = bool(config.SECURE_SSL_REDIRECT)
+
 ALLOWED_HOSTS = ['127.0.0.1', 'www.x-risk.net']
 
 SECRET_KEY = config.SECRET_KEY
